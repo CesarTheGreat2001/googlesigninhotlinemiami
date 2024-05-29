662b9b3b4dbe73e9ecc4c4f7af65b9bac084a260
CesarTheGreat2001 <82846756+CesarTheGreat2001@users.noreply.github.com>
2024-05-29 08:44:12 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: {
-    domains: ["lh3.googleusercontent.com"],
+// next.config.js
+module.exports = {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    const pathMap = {
+      ...defaultPathMap,
+      '/api/auth/[...nextauth]': { page: '/api/auth/[...nextauth]' },
+    };
+    delete pathMap['/api/auth/[...nextauth]'];
+    return pathMap;
   },
 };
-
-module.exports = nextConfig;
